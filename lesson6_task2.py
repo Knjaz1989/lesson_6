8c47a441eef7ef80df10b3062e90103779962b39
Igor Sverchkov <knjaz1989@gmail.com>
2021-08-29 17:54:53 +0300

    task2 added

---
+++ lesson6_task2.py
@@ -0,0 +1,41 @@
+class Student:
+   
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        
+    def give_rate(self, course, mentor, grade):
+        if isinstance(self, Student) and course in self.courses_in_progress and course in mentor.courses_attached:
+            if course in mentor.grades:
+                mentor.grades[course] += [grade]
+            else:
+                mentor.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
+class Mentor:
+    
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+	
+    def __init__(self, name, surname):
+		super().__init__(name,surname)
+		self.grades = {}
+	
+class Reviewer(Mentor):
+                
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
